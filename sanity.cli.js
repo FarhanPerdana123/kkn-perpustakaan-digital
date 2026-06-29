026431224797b96b98c74a66a1ff61d6f10c1433
FarhanPerdana123 <muhammadfarhanperdana@mail.ugm.ac.id>
2026-06-29 23:57:49 +0700

    Add project configuration

---
+++ sanity.cli.js
@@ -0,0 +1,9 @@
+import { defineCliConfig } from "sanity/cli";
+import { dataset, projectId } from "./src/sanity/env";
+
+export default defineCliConfig({
+  api: {
+    dataset: dataset || "production",
+    projectId: projectId || "00000000",
+  },
+});
---
+++ sanity.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { apiVersion, dataset, projectId } from "./src/sanity/env";
+import { schemaTypes } from "./src/sanity/schemaTypes";
+
+export default defineConfig({
+  name: "perpustakaan_digital_desa",
+  title: "Perpustakaan Digital Desa",
+  projectId: projectId || "00000000",
+  dataset: dataset || "production",
+  basePath: "/studio",
+  plugins: [structureTool()],
+  schema: {
+    types: schemaTypes,
+  },
+  scheduledPublishing: {
+    enabled: false,
+  },
+  document: {
+    productionUrl: async (prev, context) => {
+      const { document } = context;
+
+      if (document?._type === "book") {
+        return "/koleksi";
+      }
+
+      if (document?._type === "category" && document?.slug?.current) {
+        return `/kategori/${document.slug.current}`;
+      }
+
+      return prev;
+    },
+  },
+  apiVersion,
+});
